e2f8336e1370fa97e412b46892910f7fba13ccfb
mehanik334 <meha334@gmail.com>
2016-05-05 07:40:08 +0300

    Update DoneFragment.java

--- app/src/main/java/com/denisenko/aist2/DoneFragment.java
+++ app/src/main/java/com/denisenko/aist2/DoneFragment.java
@@ -3,6 +3,8 @@ package com.denisenko.aist2;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +17,16 @@ public class DoneFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.done_layout,null);
+        View view=inflater.inflate(R.layout.done_layout,container,false);
+        RecyclerView mRecycleView= (RecyclerView) view.findViewById(R.id.event_recycler_view_done);
+        mRecycleView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false);
+        mRecycleView.setLayoutManager(mLayoutManager);
+
+        RecyclerView.Adapter mAdapter=new RecycleAdapterFrg(new int[]{R.drawable.card_12_x, R.drawable.card_12_x1,R.drawable.card_12_x2,R.drawable.card_12_x3});
+        mRecycleView.setAdapter(mAdapter);
+        return view;
     }
 
 
