658d563b3127d16b9d28a10c0ac06e5b88dd81a6
Alexander Denisenko <meha334@gmail.com>
2016-05-08 16:33:15 +0300

    add some in DoneFragment

--- app/src/main/java/com/denisenko/aist2/DoneFragment.java
+++ app/src/main/java/com/denisenko/aist2/DoneFragment.java
@@ -1,8 +1,9 @@
 package com.denisenko.aist2;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,6 @@ import com.androidbelieve.aist2.R;
 
 public class DoneFragment extends Fragment {
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.done_layout,container,false);
